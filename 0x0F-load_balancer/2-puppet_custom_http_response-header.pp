b9d4149092aecaf29edcd588c5e728a7884fc9f1
cybernuki <1164@holbertonschool.com>
2020-04-04 18:57:28 +0000

    add content!

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,21 @@
+# just a puppet
+
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
+  ensure => installed,
+}
+
+-> file { '/var/www/html/index.html' :
+  content => 'Holberton School!',
+}
+
+-> file_line { 'adding header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+
+-> service { 'nginx':
+  ensure => running,
+}
