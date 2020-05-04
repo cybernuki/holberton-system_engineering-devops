8fadcd96177d320b85c677c1d9a263fc27c9bdb9
cybernuki <1164@holbertonschool.com>
2020-05-04 22:04:55 +0000

    unknown bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -23,48 +23,47 @@ def consume(id):
 		'usr_data': Contains a dictionary with the user information
 		'usr_todo': contains a list of dictionaries with todos information
 	"""
-    user_request = USERS_SOURCE.format(id)
-    todos_request = TODOS_SOURCE.format(id)
+	user_request = USERS_SOURCE.format(id)
+	todos_request = TODOS_SOURCE.format(id)
+	usr_response = requests.get(user_request)
+	if usr_response.status_code != 200:
+		return None
 
-    usr_response = requests.get(user_request)
-    if usr_response.status_code != 200:
-        return None
-
-    tds_response = requests.get(todos_request)
-    if tds_response.status_code != 200:
-        return None
-    return {'usr_data': usr_response.json(), 'usr_todos': tds_response.json()}
+	tds_response = requests.get(todos_request)
+	if tds_response.status_code != 200:
+		return None
+	return {'usr_data': usr_response.json(), 'usr_todos': tds_response.json()}
 
 
 def display(data):
 	"""
 		This function display user information and their todos
 	"""
-    user_name = data.get('usr_data').get('name')
-    user_todos = data.get('usr_todos')
+	user_name = data.get('usr_data').get('name')
+	user_todos = data.get('usr_todos')
 
-    if not user_name or not user_todos:
-        print("Some values are None")
-        return None
+	if not user_name or not user_todos:
+		print("Some values are None")
+		return None
 
-    total_tasks = len(user_todos)
-    done_tasks = [done for done in user_todos if done.get('completed')]
+	total_tasks = len(user_todos)
+	done_tasks = [done for done in user_todos if done.get('completed')]
 
-    print(FIRST_LINE.format(user_name, len(done_tasks), total_tasks))
-    for task in done_tasks:
-        title = task.get('title')
-        print(N_LINES_TEMPLATE.format(title))
+	print(FIRST_LINE.format(user_name, len(done_tasks), total_tasks))
+	for task in done_tasks:
+		title = task.get('title')
+		print(N_LINES_TEMPLATE.format(title))
 
 
 def main(id):
 	"""
 		Main function
 	"""
-    if not id:
-        return None
-    display(consume(id))
+	if not id:
+		return None
+	display(consume(id))
 
 
 if __name__ == "__main__":
-    id = argv[1] if len(argv) == 2 else None
-    main(id)
+	id = argv[1] if len(argv) == 2 else None
+	main(id)
