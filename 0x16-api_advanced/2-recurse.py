f11365929c5f864e4101350ec22e569c691fe1ff
cybernuki <1164@holbertonschool.com>
2020-05-08 03:10:26 +0000

    challenge 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,31 +6,19 @@ If no results are found for the given subreddit, the function should return None
 """
 import requests
 
-def recurse(subreddit, data_list=[], after=None):
-    if type(subreddit) is not str:
-        return None
-    tmp = subreddit
-    api_url = "https://api.reddit.com/r/{}/hot?after={}".format(tmp, after)
-    headers = {'user-agent': 'firefox:holberton/0.1.0'}
-    res = requests.get(api_url, headers=headers)
-    if res.status_code == 200:
-        data = res.json()["data"]["children"]
-        after = res.json()["data"]["after"]
-        if after is None:
-            data_list = titles(data, len(data))
-            return data_list
-        data_list.append(recurse(subreddit, data_list, after=after))
-        data_list = titles(data, len(data))
-    else:
-        return None
-    return data_list
+def recurse(subreddit, hot_list=[], after=""):
+    if after is None:
+        return []
 
-
-def titles(data_list, length, arr=[]):
-    """
-    Gets titles of posts from the data
-    """
-    if length == 0:
-        return arr
-    arr.append(arr[length - 1]["data"]["title"])
-    return titles(arr, length - 1, arr)
+    url_redd = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url_redd += "?limit=100&after={}".format(after)
+    headers = {'user-agent': 'request'}
+    response = requests.get(url_redd, headers=headers,
+                            allow_redirects=False)
+    if str(response) != "<Response [200]>":
+        return None
+    r_json = response.json()
+    hot_posts_json = r_json.get("data").get("children")
+    for post in hot_posts_json:
+        hot_list.append(post.get("data").get("title"))
+    return hot_list + recurse(subreddit, [], r_json.get("data").get("after"))
