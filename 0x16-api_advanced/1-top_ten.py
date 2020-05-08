5d380f40056e6499ddda822397e254445ebaedba
cybernuki <1164@holbertonschool.com>
2020-05-08 03:01:01 +0000

    challenge 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+
+
+def top_ten(subreddit):
+    data = ""
+    if type(subreddit) is not str:
+        data = "None\n"
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {'user-agent': 'chrome:cybernuki/0.1.0'}
+        res = requests.get(url, headers=headers)
+        if res.status_code != 200:
+            data = "None\n"
+        else:
+            data_json = res.json().get("data").get("children")
+            for i in range(10):
+                data_title = data_json[i].get("data").get("title")
+                data += "{}\n".format(data_title)
+    print(data, end="")
